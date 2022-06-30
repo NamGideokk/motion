3cc2df50329109863be8aa8f2c24014b89d165cb
Nam Gideok <namgd1222@gmail.com>
2022-06-30 16:13:37 +0900

    VideoComponent 정규표현식 이용하여 url 추출하는 함수  추가

--- src/components/page/item/video.ts
+++ src/components/page/item/video.ts
@@ -23,11 +23,31 @@ export class VideoComponent extends BaseComponent<HTMLElement> {
       ".video__iframe"
     )! as HTMLIFrameElement;
     iframe.src = "https://www.youtube.com/embed/uv20S1lduU4"; // url -> video ID extraction -> embed
-    console.log(url);
+    iframe.src = this.convertToEmbeddedURL(url);
 
     const titleElement = this.element.querySelector(
       ".video__title"
     )! as HTMLHeadingElement;
     titleElement.textContent = title;
   }
+
+  private convertToEmbeddedURL(url: string): string {
+    // 정규표현식 Regex 사용
+    const regExp =
+      /^(?:https?:\/\/)?(?:www\.)?(?:(?:youtube.com\/(?:(?:watch\?v=)|(?:embed\/))([a-zA-Z0-9-]{11}))|(?:youtu.be\/([a-zA-Z0-9-]{11})))/;
+    const match = url.match(regExp);
+
+    const videoId = match ? match[1] || match[2] : undefined;
+    if (videoId) {
+      return `https://www.youtube.com/embed/${videoId}`;
+    }
+    return url;
+  }
 }
+
+// 사용자의 주소 input
+// https://www.youtube.com/watch?v=uv20S1lduU4
+// https://youtu.be/uv20S1lduU4
+
+// output
+// https://www.youtube.com/embed/uv20S1lduU4
