abb1f347cab725730ecc61cc02c005ed9591cf71
Nam Gideok <namgd1222@gmail.com>
2022-07-01 00:33:17 +0900

    각 게시물 리스트를 감싸는 PageItemComponent 추가

--- dist/components/page/item/video.js
+++ dist/components/page/item/video.js
@@ -14,7 +14,7 @@ export class VideoComponent extends BaseComponent {
         titleElement.textContent = title;
     }
     convertToEmbeddedURL(url) {
-        const regExp = /^(?:https?:\/\/)?(?:www\.)?(?:(?:youtube.com\/(?:(?:watch\?v=)|(?:embed\/))([a-zA-Z0-9-]{11}))|(?:youtu.be\/([a-zA-Z0-9-]{11})))/;
+        const regExp = /^(?:https?:\/\/)?(?:www\.)?(?:(?:youtube.com\/(?:(?:watch\?v=)|(?:embed\/))([a-zA-Z0-9-|_]{11}))|(?:youtu.be\/([a-zA-Z0-9-|_]{11})))/;
         const match = url.match(regExp);
         const videoId = match ? match[1] || match[2] : undefined;
         if (videoId) {
--- src/components/page/item/video.ts
+++ src/components/page/item/video.ts
@@ -34,7 +34,7 @@ export class VideoComponent extends BaseComponent<HTMLElement> {
   private convertToEmbeddedURL(url: string): string {
     // 정규표현식 Regex 사용
     const regExp =
-      /^(?:https?:\/\/)?(?:www\.)?(?:(?:youtube.com\/(?:(?:watch\?v=)|(?:embed\/))([a-zA-Z0-9-]{11}))|(?:youtu.be\/([a-zA-Z0-9-]{11})))/;
+      /^(?:https?:\/\/)?(?:www\.)?(?:(?:youtube.com\/(?:(?:watch\?v=)|(?:embed\/))([a-zA-Z0-9-|_]{11}))|(?:youtu.be\/([a-zA-Z0-9-|_]{11})))/;
     const match = url.match(regExp);
 
     const videoId = match ? match[1] || match[2] : undefined;
